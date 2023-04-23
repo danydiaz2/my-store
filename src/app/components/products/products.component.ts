651fb2a4e95218bf3c1b193eb1e8632dc23df831
Daniel Diaz <danieldi@backbase.com>
2023-04-22 23:08:11 -0500

    add store services

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Product } from 'src/app/models/product.model';
+import { StoreService } from 'src/app/services/store.service';
 
 @Component({
   selector: 'app-products',
@@ -49,10 +50,13 @@ export class ProductsComponent {
     },
   ]
 
+  constructor( private storeService: StoreService){
+    this.myShoppingCart = this.storeService.getShoppingCart();
+  }
+
   onAddToShoppingCart(product: Product) {
-    console.log(product);
-    this.myShoppingCart.push(product);
-    this.total = this.myShoppingCart.reduce((sum, item) => sum + item.price, 0);
+    this.storeService.addProduct(product);
+    this.total = this.storeService.getTotal();
   }
 
 }
