3aeaa976f045dbc66392d834b162def1e7bc3208
Daniel Diaz <danieldi@backbase.com>
2023-04-26 20:09:12 -0500

    add environtment

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -2,13 +2,14 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { CreateProductDTO, Product, UpdateProductDTO } from '../models/product.model';
 import { retry } from 'rxjs';
+import { enviroment } from 'src/enviroment/enviroment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  private apiUrl = 'https://young-sands-07814.herokuapp.com/api/products'
+  private apiUrl = `${enviroment.API_URL}/api/products`
 
   constructor(private http: HttpClient) { }
 
