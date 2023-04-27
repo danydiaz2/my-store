2ef2f1c1253f3f3d0af8203d753d60509f360df0
Daniel Diaz <danieldi@backbase.com>
2023-04-26 20:51:52 -0500

    Manejo de errores

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -28,6 +28,7 @@ export class ProductsComponent implements OnInit {
   };
   limit = 10;
   offset = 0;
+  statusDetail: 'loading' | 'success' | 'error' | 'init' = 'init';
 
   constructor( private storeService: StoreService, private productsService: ProductsService){
     this.myShoppingCart = this.storeService.getShoppingCart();
@@ -51,10 +52,15 @@ export class ProductsComponent implements OnInit {
   }
 
   onShowDetail(id: string) {
+    this.statusDetail = 'loading';
     this.productsService.getProduct(id)
     .subscribe(data => {
       this.toggleProductDetail()
      this.productChosen = data;
+     this.statusDetail = 'success';
+    }, errorMsg => {
+      window.alert(errorMsg);
+      this.statusDetail = 'error';
     })
   }
 
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpParams, HttpStatusCode } from '@angular/common/http';
 import { CreateProductDTO, Product, UpdateProductDTO } from '../models/product.model';
-import { retry } from 'rxjs';
+import { catchError, retry, throwError } from 'rxjs';
 import { enviroment } from 'src/enviroment/enviroment';
 
 @Injectable({
@@ -27,6 +27,20 @@ export class ProductsService {
 
   getProduct(id: string) {
     return this.http.get<Product>(`${this.apiUrl}/${id}`)
+    .pipe(
+      catchError((error : HttpErrorResponse) => {
+        if (error.status === HttpStatusCode.Conflict) {
+          return throwError('Algo esta fallando en el servidor')
+        }
+        if (error.status === HttpStatusCode.NotFound) {
+          return throwError('El producto no existe')
+        }
+        if (error.status === HttpStatusCode.Unauthorized) {
+          return throwError('No estas permitido')
+        }
+        return throwError('Ups algo salió mal')
+      })
+    )
   }
 
   getProductsByPage(limit: number, offset: number) {
