a9b80c53aa063d1fd9ce5705d4e6b3045b4ac0e3
Daniel Diaz <danieldi@backbase.com>
2023-05-09 22:59:00 -0500

    Add file services and upload/download methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Product } from './models/product.model';
 import { AuthService } from './services/auth.service';
 import { UsersService } from './services/users.service';
+import { FilesService } from './services/files.service';
 
 @Component({
   selector: 'app-root',
@@ -12,8 +13,10 @@ export class AppComponent {
 
   imgParent = "";
   token = '';
+  imgRta = '';
 
-  constructor(private authService: AuthService, private userService: UsersService) {}
+  constructor(private authService: AuthService, private userService: UsersService,
+              private filesService: FilesService) {}
 
   onLoaded( img : string) {
     console.log(img);
@@ -28,5 +31,21 @@ export class AppComponent {
     .subscribe( rta => {
       console.log(rta);
     })
+  }
+
+  downloadPdf() {
+    this.filesService.getFile('my.pdf', 'https://young-sands-07814.herokuapp.com/api/files/dummy.pdf', 'application/pdf')
+    .subscribe()
+  }
+
+  onUpload(event: Event) {
+    const element = event.target as HTMLInputElement;
+    const file = element.files?.item(0);
+    if(file){
+      this.filesService.uploadFile(file)
+    .subscribe(rta => {
+      this.imgRta = rta.location;
+    })
+    }
   }
 }
