62c581f7c514fdc0c9568209b6eecc2b20becf66
Daniel Diaz <danieldi@backbase.com>
2023-05-10 21:29:15 -0500

    avoid callbackhell

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
 import { Product } from 'src/app/models/product.model';
 import { ProductsService } from 'src/app/services/products.service';
 
@@ -19,14 +20,18 @@ export class CategoryComponent implements OnInit {
 
 
   ngOnInit(): void {
-      this.route.paramMap.subscribe(params => {
-        this.categoryId = params.get('id');
-        if (this.categoryId) {
-          this.productsService.getByCategory(this.categoryId, this.limit, this.offset)
-          .subscribe(data => {
-            this.products = data;
-          })
-        }
+      this.route.paramMap
+      .pipe(
+        switchMap(params => {
+          this.categoryId = params.get('id');
+          if (this.categoryId) {
+            return this.productsService.getByCategory(this.categoryId, this.limit, this.offset)
+          }
+          return []
+        })
+      )
+      .subscribe(data => {
+        this.products = data;
       })
   }
   
